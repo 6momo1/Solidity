ff578b2830cfead3f5d1703b12aaea35ec7f2a48
Tomi Lui <tomilui1221@gmail.com>
2021-06-02 23:32:48 -0700

    ERC20Token added

---
+++ ERC20Token.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.8.4;
+
+contract ERC20Token {
+    string public name;
+    mapping(address => uint256) public balances;
+
+    constructor(string memory _name) public {
+        name = _name;
+    }
+
+    function mint() public virtual {
+        balances[tx.origin]++;
+    }
+}
+
+contract MyToken is ERC20Token {
+    string public symbol;
+    address[] public owners;
+    uint256 ownerCount;
+
+    constructor(string memory _name, string memory _symbol) ERC20Token(_name) {
+        symbol = _symbol;
+    }
+
+    function mint() public override {
+        super.mint();
+        ownerCount++;
+        owners.push(msg.sender);
+    }
+}
